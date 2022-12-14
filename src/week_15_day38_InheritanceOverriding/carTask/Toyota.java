3faf79067eb0a4a8da8b068dfa032e7936f1b502
Halit OTURAK <halithamza0920@gmail.com>
2022-12-14 13:44:35 +0300

    created super constructors

---
+++ src/week_15_day38_InheritanceOverriding/carTask/Toyota.java
@@ -0,0 +1,9 @@
+package week_15_day38_InheritanceOverriding.carTask;
+
+public class Toyota extends Car{
+
+
+    public Toyota(String brand, String model, int year, double price, String color, int miles) {
+        super(brand, model, year, price, color, miles);
+    }
+}
