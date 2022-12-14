7545d5a59e16072cb6066e3686b8776917a52a68
Halit OTURAK <halithamza0920@gmail.com>
2022-12-14 14:03:44 +0300

    created super cons

---
+++ src/week_15_day38_InheritanceOverriding/carTask/Tesla.java
@@ -0,0 +1,9 @@
+package week_15_day38_InheritanceOverriding.carTask;
+
+public class Tesla extends Car{
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+
+}
