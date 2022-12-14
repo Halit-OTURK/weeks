47297d1788c5799006f1ca4ef0396ca07f2ea008
Halit OTURAK <halithamza0920@gmail.com>
2022-12-14 14:00:52 +0300

    created super cons

---
+++ src/week_15_day38_InheritanceOverriding/carTask/BMW.java
@@ -0,0 +1,10 @@
+package week_15_day38_InheritanceOverriding.carTask;
+
+public class BMW extends Car{
+
+    public BMW( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+
+}
