66ad938c1e6e3c7ed2775185fdbb7b2bd2b5baa4
Halit OTURAK <halithamza0920@gmail.com>
2022-12-14 14:01:37 +0300

    created extra methods

--- src/week_15_day38_InheritanceOverriding/carTask/BMW.java
+++ src/week_15_day38_InheritanceOverriding/carTask/BMW.java
@@ -6,5 +6,16 @@ public class BMW extends Car{
         super("BMW", model, year, price, color, miles);
     }
 
+    public void breaksDown(){
+        System.out.println(brand + " "+ model +" is breaking down");
+    }
 
+    public void racing(){
+        System.out.println(brand + " "+ model +" is a racing car");
+    }
+
+
+    public void start(){
+        System.out.println("Call mechanic to jump start "+brand+" "+model);
+    }
 }
