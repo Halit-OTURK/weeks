f44a8210a92e1683609c3d8867176b8fa12b3328
Halit OTURAK <halithamza0920@gmail.com>
2022-12-14 14:16:54 +0300

    created cars

--- src/week_15_day38_InheritanceOverriding/carTask/ParkingLot.java
+++ src/week_15_day38_InheritanceOverriding/carTask/ParkingLot.java
@@ -3,5 +3,8 @@ package week_15_day38_InheritanceOverriding.carTask;
 public class ParkingLot {
     public static void main(String[] args) {
 
+        Toyota toyota =new Toyota("Camry", 2020, 20000, "Gray", 123456);
+        BMW Bmw =new BMW("X5", 2020, 67350, "black", 12000);
+        Tesla Tesla1= new Tesla("Model S", 2022, 94990, "Red", 0);
     }
 }
