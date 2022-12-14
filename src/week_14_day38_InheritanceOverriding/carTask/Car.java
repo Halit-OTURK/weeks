6e5ba36545543c9e481dcb92d12f28137fcc095c
Halit OTURAK <halithamza0920@gmail.com>
2022-12-14 12:52:08 +0300

    instance variables

--- src/week_14_day38_InheritanceOverriding/carTask/Car.java
+++ src/week_14_day38_InheritanceOverriding/carTask/Car.java
@@ -1,5 +1,26 @@
 package week_14_day38_InheritanceOverriding.carTask;
 
 public class Car {
+    public String brand;
+    public String model;
+    public int year;
+    public  double price;
+    public String color;
+    public int miles;
+
+
+
+
+
 
 }
+/*
+Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+
+				add a constructor to set all the fields
+
+			instance methods:
+				start(), drive(),toString()
+ */
