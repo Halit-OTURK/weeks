42e1a055c251ca4cce8a9e15fedcac5247c0c2b9
Halit OTURAK <halithamza0920@gmail.com>
2022-12-14 12:54:36 +0300

    finished

--- src/week_14_day38_InheritanceOverriding/carTask/Car.java
+++ src/week_14_day38_InheritanceOverriding/carTask/Car.java
@@ -9,10 +9,35 @@ public class Car {
     public int miles;
 
 
+    public void start(){
+        System.out.println(brand +" "+ model + " is starting");
+    }
+
+    public void drive(){
+        System.out.println(brand +" "+ model + " is driving");
+    }
 
 
 
+    public Car(String brand, String model, int year, double price, String color, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
 
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
 }
 /*
 Create a class called Car
