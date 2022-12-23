ddd61f1d95828fc858fc5addace601374ef055ca
Halit-OTURK <halithamza0920@gmail.com>
2022-12-23 11:37:52 +0300

    finished

--- src/Replit_Task_Empty/Test.java
+++ src/Replit_Task_Empty/Test.java
@@ -5,22 +5,44 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.print("Lütfen şifrenizi belirleyiniz: ");
 
-        Scanner input= new Scanner(System.in);
-        String password= input.nextLine();
+        Scanner input = new Scanner(System.in);
+        String password = input.nextLine();
+
+        if (password.length() > 7) {
+            if ((password.charAt(0) >='A' && password.charAt(0) <= 'Z')) {
+                if ((password.charAt(password.length() - 1) > 'a' && password.charAt(password.length() - 1) < 'z')) {
+                    if (!password.contains(" ")) {
+                        System.out.println("Şifre başarı ile tamamlandı");
+                    }else {
+                        System.out.println("başarısız şifre denemesi. Lütfen tekrar deneyiniz!");
+                    }
+                }else {
+                    System.out.println("başarısız şifre denemesi. Lütfen tekrar deneyiniz!");
+                }
+            }else {
+                System.out.println("başarısız şifre denemesi. Lütfen tekrar deneyiniz!");
+            }
+
+        } else {
+            System.out.println("başarısız şifre denemesi. Lütfen tekrar deneyiniz!");
+        }
+
+        System.out.println("ikisini de düzelttim");
 
         if (password.length()<8){
             System.out.println("işlem başarısız lütfen yeni bir şifre girin");
             System.exit(0);
-                   }
+        }
 
-        if (!(password.charAt(0)>'A'|| password.charAt(0)<'Z')){
+        if (!(password.charAt(0)>='A'&& password.charAt(0) <='Z')){
             System.out.println("işlem başarısız lütfen yeni bir şifre girin");
             System.exit(0);
         }
 
 
-        if (!(password.charAt(password.length()-1)>'a'|| password.charAt(password.length()-1)<'z')){
+        if (!(password.charAt(password.length()-1)>'a' && password.charAt(password.length()-1)<'z')){
             System.out.println("işlem başarısız lütfen yeni bir şifre girin");
             System.exit(0);
         }
@@ -29,5 +51,8 @@ public class Test {
             System.exit(0);
         }
         System.out.println("Şifre başarı ile tamamlandı");
+
+
+
     }
 }
---
+++ src/week_16_day41_Exceptions/MultiCatchBlocks.java
@@ -0,0 +1,44 @@
+package week_16_day41_Exceptions;
+
+import week_15_day39_Recap.cydeoTask.Employee;
+
+public class MultiCatchBlocks {
+
+    public static void main(String[] args) {
+        Employee employee = null;
+
+        try {
+
+            System.out.println(employee.getSalary()); // Null Pointer Exception
+
+        }catch (NullPointerException e){
+            System.out.println("First Catch Block");
+            e.printStackTrace();
+        }catch(IndexOutOfBoundsException e){
+            System.out.println("Second catch block");
+            e.printStackTrace();
+        }catch (ArithmeticException e){
+            System.out.println("Third catch block");
+            e.printStackTrace();
+        }catch (ClassCastException e){
+            System.out.println("Fourth catch block");
+            e.printStackTrace();
+        }catch (RuntimeException e){
+            System.out.println("Fifth catch block");
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Tested Completed");
+
+
+        System.out.println("=>".repeat(55));
+
+        try {
+            System.out.println("Java".charAt(-1));
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+
+    }
+}
