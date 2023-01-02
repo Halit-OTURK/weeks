aedb4b183f00a268feb590fda9c18e58dc1c49bc
Halit-OTURK <halithamza0920@gmail.com>
2023-01-03 00:44:23 +0300

    finished

---
+++ src/week_18_day44_Abstraction/animalTask/Flyable.java
@@ -0,0 +1,9 @@
+package week_18_day44_Abstraction.animalTask;
+
+public interface Flyable {
+
+    boolean canFly=true;
+
+    void fly();
+
+}
---
+++ src/week_18_day44_Abstraction/animalTask/WildAnimal.java
@@ -0,0 +1,8 @@
+package week_18_day44_Abstraction.animalTask;
+
+public interface WildAnimal {
+
+    boolean isWild=true;
+
+    void hunt();
+}
